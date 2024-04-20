66965de538a4c5fd2459a0e89074f4f020a781db
durgesh awashti <133563743+awasthi16@users.noreply.github.com>
2024-04-20 13:44:36 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
 import "./App.css";
+import { arc } from "d3";
 
 function App() {
-  const width=960;
-  const height=500
-  const centerX=width/2
-  const centerY=height/2
-  const strokeWidth= 20
-  const eyeffsetX=90
-  const eyeffsetY=100
-  const eyeRadius=40
+  const width = 960;
+  const height = 500;
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const strokeWidth = 20;
+  const eyeffsetX = 90;
+  const eyeffsetY = 100;
+  const eyeRadius = 40;
+  const mouthWidth = 20;
+  const mouthRadious = 140;
+
+  const mouthArc = arc()
+    .innerRadius(mouthRadious)
+    .outerRadius(mouthRadious + mouthWidth)
+    .startAngle(Math.PI / 2)
+    .endAngle((Math.PI * 3) / 2);
+
   return (
     <div>
       <svg width={width} height={height}>
-        <circle
-          cx={centerX}
-          r={centerY-strokeWidth/2}
-          cy={centerY}
-          fill="yellow"
-          stroke="black"
-          stroke-width={strokeWidth}
-        ></circle>
-        <circle cx={centerX-eyeffsetX} r={eyeRadius} cy={centerY-eyeffsetY}></circle>
-        <circle cx={centerX+eyeffsetX} r={eyeRadius} cy={centerY-eyeffsetY}></circle>
-        
+        <g transform={`translate(${centerX},${centerY})`}>
+          <circle
+            r={centerY - strokeWidth / 2}
+            fill="yellow"
+            stroke="black"
+            stroke-width={strokeWidth}
+          ></circle>
+          <circle cx={eyeffsetX} r={eyeRadius} cy={-eyeffsetY}></circle>
+          <circle cx={-eyeffsetX} r={eyeRadius} cy={-eyeffsetY}></circle>
+
+          <path d={mouthArc()} />
+        </g>
       </svg>
     </div>
   );
